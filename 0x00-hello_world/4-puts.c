68487561fac8c7f3d9cda89e35b2f652757cc2f0
rossafifati0 <fatimazahraerossafi@gmail.com>
2023-06-15 04:02:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle,followed by a newline."
+ * Return: Always 0.
  */
 int main(void)
 {       
