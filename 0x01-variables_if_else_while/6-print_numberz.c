2e150f2fafc5edb440da7717cf218abe0b801f6a
rossafifati0 <fatimazahraerossafi@gmail.com>
2023-06-16 12:44:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		putchar(number);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
