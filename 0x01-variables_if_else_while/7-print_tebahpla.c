85acdf4e46a0dd888330b443c5316f2488b51219
rossafifati0 <fatimazahraerossafi@gmail.com>
2023-06-16 12:46:16 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter >= 97; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
