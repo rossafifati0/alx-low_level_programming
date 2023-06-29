623ad17d3d1c5d287d90c9967149d084008b7a04
rossafifati0 <fatimazahraerossafi@gmail.com>
2023-06-29 09:03:03 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
+    int a[5];
+    int *p;
+
+    a[2] = 1024;
+    p = &a[2] - 2;
+
+    *(p + 2) = 98;  /* Added line */
+
+    printf("a[2] = %d\n", a[2]);
+
     return (0);
 }
+
